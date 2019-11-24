a05bbfc9f515b591dc3134c609fca9e4b607d434
Ali Tamoor <alitamoor1998@gmail.com>
2019-11-24 17:50:18 +0500

    "OPTIMAL PATH Added"
    Optimal Path also included

--- tsp1.cpp
+++ tsp1.cpp
@@ -3,6 +3,13 @@
 #include <bits/stdc++.h> 
 using namespace std; 
 int no;
+int travellingSalesmanProblem( int s);
+int main() 
+{ 	
+	int s = 0; 
+	cout << travellingSalesmanProblem(s) << endl; 
+	return 0; 
+} 
 
 int travellingSalesmanProblem( int s) 
 { 
@@ -19,7 +26,7 @@ int travellingSalesmanProblem( int s)
 		cin>>cities[i];
 	}
 	
-	
+	cout<<"\n\t\t\t E N T E R	 C O S T S\n\n";
 	for(int i=0;i<no;i++)
 	{
 		for(int j=0;j<no;j++)
@@ -28,13 +35,14 @@ int travellingSalesmanProblem( int s)
 			{
 				continue;
 			}
-			cout<<"Cost from "<<cities[i]<<" to "<<cities[j]<<" : ";
+			cout<<endl<<"Cost from "<<cities[i]<<" to "<<cities[j]<<" : ";
 			cin>>graph[i][j];
 		}
 	}
 	
 	
 	cout<<"\n\n";
+	cout<<"\n\t\t\t M A T R I X		F O R M E D \n\n";
 	for(int i=0;i<no;i++)
 	{
 		cout<<"\n\n";
@@ -45,36 +53,81 @@ int travellingSalesmanProblem( int s)
 	}
 	
 	
-	cout<<"\n";
+		cout<<"\n";
+		cout<<"\nEnter Starting City : ";
+		cin>>s;
+		if(s>=0 || s<no)
+		{
+			cout<<endl;
+			
+		}
+		else
+		{
+			cout<<"INVALID INPUT";
+			return 0;
+		}
+		s=s-1;
 	vector<int> vertex; 
 	for (int i = 0; i < no; i++) 
 		if (i != s) 
-			vertex.push_back(i); 
+			vertex.push_back(i);
+			
+	int low_path[(const int)(no+1)]={0}; 
 	int min_path = INT_MAX; 
 	
 	do { 
 		int current_pathweight = 0; 
-	 
+	 	int path[(const int)(no+1)]={0};
 		int k = s; 
+		path[0]=s+1;
 		for (int i = 0; i < vertex.size(); i++) { 
+			//cout<<vertex[i]<<endl;
+			
+			//cout<<cities[k]<<endl;
+			//cout<<cities[vertex[i]]<<endl;
+			//cout<<cities[vertex[k]]<<endl;
+			//cout<<"-"<<endl;
+			path[i+1]=vertex[i]+1;
 			current_pathweight += graph[k][vertex[i]]; 
 			k = vertex[i]; 
 		} 
 		current_pathweight += graph[k][s]; 
-
+		int chk=min_path;
 		// update minimum 
-		min_path = min(min_path, current_pathweight);
-		 
-		
+		 if (min_path>current_pathweight)
+		 {
+		 	min_path =current_pathweight;
+		 }
+        if(min_path!=chk)
+		{
+			for(int j=0;j<no;j++)
+				{
+					low_path[j]=path[j];
+				}
+		}
 	} while (next_permutation(vertex.begin(), vertex.end())); 
+	
+	cout<<"\nPath : ";
+	low_path[no]=s+1;
+	int temp=0;
+	for(int i=0;i<=no;i++)
+	{
+		if(i!=0)
+		{
+			cout<<"---->";
+		}
+		//cout<<low_path[i];
+		//cout<<"-----------"<<endl;
+		temp=low_path[i]-1;
+		//cout<<"temp: "<<temp;
+		cout<<cities[temp];
+		//cout<<"-----------"<<endl;
+	}
 
+	cout<<endl;
+	cout<<"COST: ";
 	return min_path; 
 } 
 
-int main() 
-{ 	
-	int s = 0; 
-	cout << travellingSalesmanProblem(s) << endl; 
-	return 0; 
-} 
+
 
